513427f46c7f7f16640d80ef0b89884b584fd9af
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-08-05 12:01:04 -0300

    improvement: Turning pages into array to improve scalability

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import { getAccessToken } from "@/actions/credentials";
-import PageOne from "@/components/pages/one";
-import PageThree from "@/components/pages/three";
-import PageTwo from "@/components/pages/two";
+import CompleteInput from "@/components/complete-input";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { pages } from "@/data/pages";
 import { inputStore, pageStore } from "@/store";
 import { useQuery } from "@tanstack/react-query";
 import { redirect } from "next/navigation";
@@ -22,14 +21,21 @@ export default function Home() {
 
   if (!accessToken) redirect("/login");
 
-  const pages = [PageOne, PageTwo, PageThree].map((Element) => (
-    <Element key={Element.toString()} store={inputs} />
-  ));
-
   return (
     <div>
       <Card>
-        <CardContent className="flex flex-col gap-6">{pages[page]}</CardContent>
+        <CardContent className="flex flex-col gap-3">
+          {pages[page].map(({ id, label, placeholder }) => (
+            <CompleteInput
+              key={id}
+              id={id}
+              label={label}
+              placeholder={placeholder}
+              onChange={(e) => inputs.setInputValue(id, e.target.value)}
+              value={(inputs[id] as string) || ""}
+            />
+          ))}
+        </CardContent>
         <CardFooter className="flex justify-between">
           <Button
             className="cursor-pointer"
---
+++ data/pages.ts
@@ -0,0 +1,33 @@
+export const pages = [
+  [
+    {
+      id: "firstName",
+      label: "First name",
+      placeholder: "Type your first name",
+    },
+    {
+      id: "lastName",
+      label: "Last name",
+      placeholder: "Type your last name",
+    },
+  ],
+  [
+    {
+      id: "productName",
+      label: "Product name",
+      placeholder: "Type your product name",
+    },
+  ],
+  [
+    {
+      id: "companyName",
+      label: "Company",
+      placeholder: "Type company name",
+    },
+    {
+      id: "jobTitle",
+      label: "Job title",
+      placeholder: "Type job title",
+    },
+  ],
+];
