54c006b2c017776e9b7079c52e075816ad5a26d3
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-08-30 12:56:37 -0300

    fix: Now trying to use next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/public/*": ["./public/template.html"],
+  },
 };
 
 export default nextConfig;
