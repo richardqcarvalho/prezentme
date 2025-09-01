6f290525a735759f99cc3b92d861fd2dfbc19156
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-09-01 11:21:51 -0300

    fix: Removing any type

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,10 @@
+import {
+  EducationT,
+  ExperienceT,
+  LanguageT,
+  ProjectT,
+  SetupT,
+} from "@/types/information";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -5,22 +12,22 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function getLanguage(languages: any[]) {
+export function getLanguage(languages: LanguageT[]) {
   return "";
 }
 
-export function getExperience(experiences: any[]) {
+export function getExperience(experiences: ExperienceT[]) {
   return "";
 }
 
-export function getEducation(educations: any[]) {
+export function getEducation(educations: EducationT[]) {
   return "";
 }
 
-export function getProject(projects: any[]) {
+export function getProject(projects: ProjectT[]) {
   return "";
 }
 
-export function getSetup(setups: any[]) {
+export function getSetup(setups: SetupT[]) {
   return "";
 }
--- types/information.ts
+++ types/information.ts
@@ -1,3 +1,36 @@
+export type LanguageT = {
+  name: string;
+  level: string;
+};
+
+type TechnologyT = {
+  name: string;
+};
+
+export type ExperienceT = {
+  title: string;
+  start: string;
+  end: string;
+  description: string;
+  technologies: TechnologyT[];
+};
+
+export type EducationT = {
+  title: string;
+  description: string;
+};
+
+export type ProjectT = {
+  title: string;
+  description: string;
+  technologies: TechnologyT[];
+};
+
+export type SetupT = {
+  name: string;
+  specs: string;
+};
+
 export type InformationsT = {
   firstName: string;
   lastName: string;
@@ -5,9 +38,9 @@ export type InformationsT = {
   number: string;
   location: string;
   email: string;
-  language: string[];
-  experience: string[];
-  education: string[];
-  project: string[];
-  setup: string[];
+  language: LanguageT[];
+  experience: ExperienceT[];
+  education: EducationT[];
+  project: ProjectT[];
+  setup: SetupT[];
 };
