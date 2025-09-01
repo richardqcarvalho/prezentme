ca81b883b1094a8cd64418c2120911bd296d5f8f
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-09-01 16:54:11 -0300

    fix: Adding missing font subsets

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 const lato = Lato({
   weight: ["100", "300", "400", "700", "900"],
   variable: "--font-lato",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
