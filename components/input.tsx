0d5c2c80e3e7b3134d74f5b4f9765447b82ac9a2
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-09-02 12:00:16 -0300

    style: Using base size to input texts

--- components/input.tsx
+++ components/input.tsx
@@ -5,12 +5,10 @@ export default function Input(
 ) {
   return (
     <div className="flex w-full flex-col gap-2">
-      <label htmlFor={props.id} className="text-[12pt]">
-        {props.label}
-      </label>
+      <label htmlFor={props.id}>{props.label}</label>
       <input
         id={props.id}
-        className="w-full rounded-lg border px-4 py-2 text-[16pt] disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+        className="w-full rounded-lg border px-4 py-2 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
         {...props}
       />
     </div>
