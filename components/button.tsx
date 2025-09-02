06ee6d8753a22a62b9d72a2bad750dcf99b36d63
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-09-02 11:59:53 -0300

    style: Improving button hover

--- components/button.tsx
+++ components/button.tsx
@@ -6,7 +6,7 @@ export default function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
     <button
       {...props}
       className={cn([
-        "flex cursor-pointer items-center justify-center gap-2 rounded-lg bg-black px-4 py-2 text-white hover:bg-black/90 disabled:pointer-events-none disabled:opacity-50",
+        "flex cursor-pointer items-center justify-center gap-2 rounded-lg bg-black px-4 py-2 text-white hover:bg-black/70 disabled:pointer-events-none disabled:opacity-50",
         props.className,
       ])}
     />
